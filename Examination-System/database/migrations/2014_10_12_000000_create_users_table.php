50da69fcf068f1893ae833222f749dcd1cbc1968
ahmdbhgat4 <ahmdbhgat4@gmail.com>
2023-06-23 03:29:04 +0300

    migrations have been created!

--- Examination-System/database/migrations/2014_10_12_000000_create_users_table.php
+++ Examination-System/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('uid')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->default(true);
+            $table->enum('type',['admin','teacher','student'])->default('teacher');
+            $table->enum('gender',['male','female']);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ Examination-System/database/migrations/2023_06_22_201057_create_exams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('exam_name');
+            $table->text('exam_desc')->nullable();
+            $table->string('exam_code');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};
